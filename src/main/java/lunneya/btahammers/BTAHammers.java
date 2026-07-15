ac4354c58e213ad283a9a281e5e03b25a784ea53
lunneya <lunneya.nivarre@gmail.com>
2026-07-15 17:21:27 +0500

    A working version of an iron digging hammer was created 3x3

--- src/main/java/lunneya/btahammers/BTAHammers.java
+++ src/main/java/lunneya/btahammers/BTAHammers.java
@@ -1,16 +1,99 @@
 package lunneya.btahammers;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.item.material.ToolMaterial;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.HalpLibe;
+import turniplabs.halplibe.event.defs.CommonEvents;
+import turniplabs.halplibe.util.dependency.Key;
+
+import lunneya.btahammers.item.ItemHammer;
+import net.minecraft.core.item.Item;
+
+import net.minecraft.client.render.item.model.ItemModelDispatcher;
+import net.minecraft.client.render.item.model.ItemModelStandard;
+
+import turniplabs.halplibe.helper.RecipeBuilder;
+import net.minecraft.core.item.Items;
+
+import net.minecraft.core.data.registry.Registries;
 
 public class BTAHammers implements ModInitializer {
 
-	public static final String MOD_ID = "btahammers";
+	public static Item IRON_HAMMER;
+
+
+	public static final String MOD_ID = HalpLibe.registerMod("btahammers", true);
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("[BTA Hammers] Mod loaded successfully!");
+		CommonEvents.BEFORE_GAME_START.listen(
+			Key.of(MOD_ID),
+			this::beforeGameStart
+		);
+
+		CommonEvents.AFTER_GAME_START.listen(
+			Key.of(MOD_ID),
+			this::afterGameStart
+		);
+
+		LOGGER.info("[BTA Hammers] Mod initialized!");
+	}
+
+	private void afterGameStart() {
+		LOGGER.info("[BTA Hammers] Registering hammer model...");
+
+		ItemModelDispatcher.getInstance().addDispatch(
+			new ItemModelStandard(IRON_HAMMER)
+				.setDisplayPos(
+					"firstperson_righthand",
+					ItemModelDispatcher.HANDHELD_FIRST_PERSON_RIGHT_HAND
+				)
+				.setDisplayPos(
+					"firstperson_lefthand",
+					ItemModelDispatcher.HANDHELD_FIRST_PERSON_LEFT_HAND
+				)
+				.setDisplayPos(
+					"thirdperson_righthand",
+					ItemModelDispatcher.HANDHELD_THIRD_PERSON_RIGHT_HAND
+				)
+				.setDisplayPos(
+					"thirdperson_lefthand",
+					ItemModelDispatcher.HANDHELD_THIRD_PERSON_LEFT_HAND
+				)
+		);
+
+		LOGGER.info("[BTA Hammers] Hammer model registered!");
+
+		// Регистрируем рецепт молота
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape(
+				"III",
+				"ISI",
+				" S "
+			)
+			.addInput('I', Items.INGOT_IRON)
+			.addInput('S', Items.STICK)
+			.create("iron_hammer", IRON_HAMMER.getDefaultStack());
+
+		// Сбрасываем кэш рецептов, чтобы BTA увидела новый рецепт
+		Registries.RECIPES.invalidateCaches();
+
+		LOGGER.info("[BTA Hammers] Iron Hammer recipe registered!");
+	}
+
+	private void beforeGameStart() {
+		LOGGER.info("[BTA Hammers] Registering hammer items...");
+
+		IRON_HAMMER = new ItemHammer(
+			"iron_hammer",
+			MOD_ID + ":item/iron_hammer",
+			20000,
+			ToolMaterial.iron
+		);
+
+		LOGGER.info("[BTA Hammers] Iron Hammer registered!");
 	}
 }
---
+++ src/main/java/lunneya/btahammers/item/ItemHammer.java
@@ -0,0 +1,165 @@
+package lunneya.btahammers.item;
+
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.Blocks;
+import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.entity.Mob;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.item.material.ToolMaterial;
+import net.minecraft.core.item.tool.ItemToolPickaxe;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
+import net.minecraft.core.world.pos.TilePos;
+import net.minecraft.core.world.pos.TilePosc;
+import org.jetbrains.annotations.NotNull;
+
+public class ItemHammer extends ItemToolPickaxe {
+
+	public ItemHammer(
+		@NotNull String name,
+		@NotNull String namespaceId,
+		int id,
+		@NotNull ToolMaterial toolMaterial
+	) {
+		super(name, namespaceId, id, toolMaterial);
+	}
+
+	@Override
+	public boolean onBlockDestroyed(
+		@NotNull ItemStack selfStack,
+		@NotNull World world,
+		@NotNull Mob mob,
+		@NotNull Block<?> removedBlock,
+		@NotNull TilePosc blockPos,
+		@NotNull Side side
+	) {
+		// Обычный износ инструмента — только один раз,
+		// за центральный блок.
+		boolean result = super.onBlockDestroyed(
+			selfStack,
+			world,
+			mob,
+			removedBlock,
+			blockPos,
+			side
+		);
+
+		// Блоки ломает игрок, поэтому для дропа нам нужен Player.
+		if (!(mob instanceof Player player)) {
+			return result;
+		}
+
+		breakArea3x3(world, player, blockPos, side);
+
+		return result;
+	}
+
+	private void breakArea3x3(
+		@NotNull World world,
+		@NotNull Player player,
+		@NotNull TilePosc center,
+		@NotNull Side side
+	) {
+		for (int a = -1; a <= 1; a++) {
+			for (int b = -1; b <= 1; b++) {
+
+				// Центральный блок уже сломан обычной механикой игры.
+				if (a == 0 && b == 0) {
+					continue;
+				}
+
+				TilePos targetPos;
+
+				switch (side) {
+					// Пол или потолок:
+					// ломаем плоскость X/Z.
+					case TOP:
+					case BOTTOM:
+						targetPos = new TilePos(
+							center.x() + a,
+							center.y(),
+							center.z() + b
+						);
+						break;
+
+					// Северная или южная стена:
+					// ломаем плоскость X/Y.
+					case NORTH:
+					case SOUTH:
+						targetPos = new TilePos(
+							center.x() + a,
+							center.y() + b,
+							center.z()
+						);
+						break;
+
+					// Западная или восточная стена:
+					// ломаем плоскость Y/Z.
+					case WEST:
+					case EAST:
+						targetPos = new TilePos(
+							center.x(),
+							center.y() + a,
+							center.z() + b
+						);
+						break;
+
+					default:
+						continue;
+				}
+
+				breakExtraBlock(world, player, targetPos, side);
+			}
+		}
+	}
+
+	private void breakExtraBlock(
+		@NotNull World world,
+		@NotNull Player player,
+		@NotNull TilePos targetPos,
+		@NotNull Side side
+	) {
+		Block<?> block = world.getBlockType(targetPos);
+
+		// Воздух пропускаем.
+		if (block == Blocks.AIR) {
+			return;
+		}
+
+		// Не ломаем принципиально неразрушаемые блоки
+		// вроде bedrock.
+		if (block.getHardness() < 0.0F) {
+			return;
+		}
+
+		int data = world.getBlockData(targetPos);
+		TileEntity tileEntity = world.getTileEntity(targetPos);
+
+		// Удаляем блок из мира.
+		if (!world.setBlockTypeNotify(targetPos, Blocks.AIR)) {
+			return;
+		}
+
+		// Повторяем стандартную логику разрушения блока игроком.
+		block.onDestroyedByPlayer(
+			world,
+			targetPos,
+			side,
+			data,
+			player,
+			this
+		);
+
+		// Обычный дроп, если текущий режим игры его предусматривает.
+		if (player.getGamemode().hasItemDrops()) {
+			block.onHarvest(
+				world,
+				player,
+				targetPos,
+				data,
+				tileEntity
+			);
+		}
+	}
+}
